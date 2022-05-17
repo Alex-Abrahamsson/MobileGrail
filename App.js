c675e3aeee0649ef53e6a201f51becc81c217380
Alexander Abrahamsson <79103208+Alex-Abrahamsson@users.noreply.github.com>
2022-05-17 23:49:02 +0200

    Setup

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textz}>Welcome to Diablo II HolyGrail!</Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert("Simple Button pressed")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#260205",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  textz: { 
+    color: "#fff",
+  }
 });
